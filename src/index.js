d498eb1f53d518837a65eba90b6a1d316f570982
Yanisdje <64096824+Yanisdje@users.noreply.github.com>
2024-05-09 13:48:35 -0400

    Final pizza project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,174 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//functions have to start with upaercase
+//each component can return only one component
+//we cannot nest the declaration itself inside, very bad idea.
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+const Header = () => {
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza co.</h1>
+    </header>
+  );
+};
+
+/*Props are very powerful, they allow us to simply enter data for 
+each element without wasting any time! They pass data form parent 
+components to child components (down the component tree).
+They literally customize components.
+Anything can be passed as  including react components!!!
+-Props are immutable.
+-If you need to mutate  then you need to use state.
+-Mutating would affect parent, creating [side effects].
+-React is all about pure functions, meaning functions without [side effects].
+-This allows for predictability.
+-One-way data flow, data can flow from parent to children and not the opposite way in react!
+*/
+
+const Menu = () => {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from out stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu!</p>
+      )}
+    </main>
+  );
+};
+
+function Pizza({ pizzaObj }) {
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+
+        {pizzaObj.soldOut ? (
+          <span>Sold out!</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )}
+      </div>
+    </li>
+  );
+}
+
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed!");
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closingHour={closeHour} />
+      ) : (
+        <p>
+          We're closed, opening hours from {openHour}:00pm to {closeHour - 12}
+          :00pm.
+        </p>
+      )}
+    </footer>
+  );
+};
+
+const Order = (props) => {
+  return (
+    <div className="order">
+      <p>We're open until {props.closingHour - 12}:00pm.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+};
+
+//functions need to reuturn some form of markup, cannot return nothing
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
